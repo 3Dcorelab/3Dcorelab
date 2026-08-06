8b8a13fda229d5d4398d1bf4cfd3e50da50cef6a
3Dcorelab <mehtiogullariyusufayaz869@gmail.com>
2026-08-06 13:37:43 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function order(product, price){
-  const text = `Merhaba 3DCoreLab, ${product} ürününü sipariş vermek istiyorum. Fiyat: ${price}.`;
-  window.open("https://wa.me/905516305006?text=" + encodeURIComponent(text), "_blank");
-}
